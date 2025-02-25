ed93f14c9a7ff564d193ad49d19783440f858b67
Yago de Lima França <134020465+Yago-Lima@users.noreply.github.com>
2025-02-25 09:48:28 -0300

    mediaNotaTodosEstudantesCurso implementado

--- src/main/java/exercicios/Aula04.java
+++ src/main/java/exercicios/Aula04.java
@@ -62,8 +62,11 @@ public class Aula04 extends Aula {
     }
 
     protected double mediaNotaTodosEstudantesCurso(@NonNull final Stream<Estudante> stream, @NonNull final Curso curso){
-        // TODO: Você precisa implementar este método. Apague estas linhas e escreva o código correto.
-        return -1;
+        return stream
+                .filter(estudante -> estudante.getCurso().equals(curso))
+                .mapToDouble(Estudante::getNota)
+                .average()
+                .orElse(-1);
     }
 
     protected double maiorNotaTodosEstudantes(@NonNull final Stream<Estudante> stream){
